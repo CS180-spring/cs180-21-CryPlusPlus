b5d776ceecdd8ed3bbc862f4fefb0573c757b08c
evarg017-2001 <evarg017@ucr.edu>
2023-05-01 11:51:18 -0700

    ev for pulling

---
+++ src/WriteData.h
@@ -0,0 +1,25 @@
+#ifndef WRITE_DATA
+#define WRITE_DATA
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "../lib/json.hpp"
+
+using json = nlohmann::ordered_json;
+
+// using namespace std;
+
+class writeData {
+    protected:
+        std::vector<std::string> headers;
+        std::string fileName;
+    public:
+        virtual ~writeData() {};
+        virtual void writeToFile(const json& obj, const std::string & fileName) = 0;
+};
+
+
+#endif
