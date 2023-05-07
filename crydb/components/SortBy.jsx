429e2ea1292a08ad9c4476f49b875a469c0af3ea
daniel-u-c <74112469+daniel-u-c@users.noreply.github.com>
2023-05-07 13:51:38 -0700

    Made Sort By component

--- crydb/components/SortBy.jsx
+++ crydb/components/SortBy.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import DropdownMenu from "./DropdownMenu";
+import TextInput from "./TextInput";
 
-const SortBy = () => {
+const SortBy = ({ fields }) => {
+  const ordering = ['ascending order', `descending order`];
   return (
-    <div>SortBy</div>
+    <div className="flex flex-col gap-2 text-sm font-inter">
+      <div className="font-bold">
+        Sort By
+      </div>
+      <DropdownMenu options={fields} />
+      in
+      <DropdownMenu options={ordering} />
+    </div>
   )
 }
 
