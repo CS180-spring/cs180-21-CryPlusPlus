0014397397dcc3e38fb3a1c6c354c785867fa182
daniel-u-c <74112469+daniel-u-c@users.noreply.github.com>
2023-05-07 02:16:51 -0700

    Implemented Table component

--- crydb/components/Table.jsx
+++ crydb/components/Table.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client";
 
-const Table = () => {
+import React from 'react'
+import { useTable } from "react-table";
+
+// {
+//   firstName: "Daniel",
+//   lastName: "Castaneda",
+//   age: 20,
+//   year: 3,
+//   major: "Computer Science",
+// }
+
+const Table = ({ columns, data }) => {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({columns, data});
+
   return (
-    <div>Table</div>
-  )
-}
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()} className='font-inter px-8 py-2 border-y-2'>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map(row => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => (
+                <td {...cell.getCellProps()} className='font-inter'>{cell.render("Cell")}</td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 export default Table
