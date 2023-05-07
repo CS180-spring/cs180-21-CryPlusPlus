cb736d3aa601df0af48d29e05bb3e08af7ffd724
daniel-u-c <74112469+daniel-u-c@users.noreply.github.com>
2023-05-07 01:10:26 -0700

    Created main page

--- crydb/app/page.jsx
+++ crydb/app/page.jsx
@@ -1,11 +1,21 @@
-'use client';
-
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div className='bg-figma-purple w-full h-40 mb-44' />
+      <div className='flex flex-col w-full font-inter items-center justify-center gap-4'>
+        <div className='font-bold text-4xl'>
+          Welcome to CryDB
+        </div>
+        <div className='font-bold text-xl'>
+          Stop crying over your data
+        </div>
+        <Link className='bg-figma-lightpurple rounded px-4 py-2 mt-4' href='main'>
+          Get Started
+        </Link>
+      </div>
+    </div>
   )
 }
 
