c9e36ac8fe46c7f1bb87fefa0d9a5abee5103e25
daniel-u-c <74112469+daniel-u-c@users.noreply.github.com>
2023-05-13 22:03:13 -0700

    Updated Menu options

--- crydb/components/Menu.jsx
+++ crydb/components/Menu.jsx
@@ -2,7 +2,9 @@ import Link from 'next/link';
 import { HiHome } from 'react-icons/Hi';
 import { AiFillFileText } from 'react-icons/Ai';
 import { MdDeleteForever } from 'react-icons/Md';
-import { BiExport } from 'react-icons/Bi';
+import { BiExport, BiSave } from 'react-icons/Bi';
+import { BsFillCollectionFill } from 'react-icons/Bs'
+import { ImCross } from 'react-icons/Im'
 
 const Menu = () => {
   return (
@@ -12,28 +14,28 @@ const Menu = () => {
         Home
       </Link>
       <button className='flex flex-row items-center gap-2 font-inter py-2 hover:bg-figma-black-grey300 w-full'>
+        <BsFillCollectionFill />
+        Create Collection
+      </button>
+      <button className='flex flex-row items-center gap-2 font-inter py-2 hover:bg-figma-black-grey300 w-full'>
+        <ImCross />
+        Delete Collection
+      </button>
+      <button className='flex flex-row items-center gap-2 font-inter py-2 hover:bg-figma-black-grey300 w-full'>
         <AiFillFileText />
-        Add Document
+        Add Document(s)
       </button>
       <button className='flex flex-row items-center gap-2 font-inter py-2 hover:bg-figma-black-grey300 w-full'>
         <MdDeleteForever />
         Delete Document
       </button>
       <button className='flex flex-row items-center gap-2 font-inter py-2 hover:bg-figma-black-grey300 w-full'>
+        <BiSave />
+        Save Changes
+      </button>
+      <button className='flex flex-row items-center gap-2 font-inter py-2 hover:bg-figma-black-grey300 w-full'>
         <BiExport />
-        Export Document
-      </button>
-      <button className='flex flex-row items-center gap-2 font-inter py-2 hover:bg-figma-black-grey300 w-full'>
-        ...
-      </button>
-      <button className='flex flex-row items-center gap-2 font-inter py-2 hover:bg-figma-black-grey300 w-full'>
-        ...
-      </button>
-      <button className='flex flex-row items-center gap-2 font-inter py-2 hover:bg-figma-black-grey300 w-full'>
-        ...
-      </button>
-      <button className='flex flex-row items-center gap-2 font-inter py-2 hover:bg-figma-black-grey300 w-full'>
-        ...
+        Export Table
       </button>
     </div>
   )
