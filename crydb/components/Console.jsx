46f5313a9f95f631d82a0531ce73ec25079ce72d
daniel-u-c <74112469+daniel-u-c@users.noreply.github.com>
2023-05-29 01:36:33 -0700

    Updated styling

--- crydb/components/Console.jsx
+++ crydb/components/Console.jsx
@@ -7,14 +7,14 @@ const Console = () => {
 
   return (
     <div className="flex flex-col h-full p-8 gap-5">
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 text-2xl">
         {consoleLogs.map((response, i) => {
           return (
             <div className="flex flex-row gap-4 hover:bg-figma-lightpink w-full" key={i}>
                 <div className='font-bold'>
                     {convertTimeToString(response.time)}:
                 </div>
-                <div className=''>
+                <div className='w-full'>
                     {response.message} 
                 </div>
             </div>
