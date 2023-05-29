49c85ebc0b26aea62e9ec0b2f6ebd647631ba351
daniel-u-c <74112469+daniel-u-c@users.noreply.github.com>
2023-05-29 00:38:38 -0700

    Added helper functions for logging

---
+++ crydb/utils/helpers.js
@@ -0,0 +1,30 @@
+import { useUserContext } from "@app/context/UserContext";
+
+const convertTimeToString = (timeT) => {
+    const date = new Date(timeT * 1000); // Convert time_t to milliseconds
+  
+    const month = (date.getMonth() + 1).toString();
+    const day = date.getDate().toString();
+    const year = date.getFullYear();
+  
+    const hour = date.getHours().toString().padStart(2, '0');
+    const minute = date.getMinutes().toString().padStart(2, '0');
+    const second = date.getSeconds().toString().padStart(2, '0');
+  
+    const ampm = hour >= 12 ? 'pm' : 'am';
+    const hour12 = (hour % 12 || 12).toString();
+  
+    const formattedDate = `${month}/${day}/${year}`;
+    const formattedTime = `${hour12}:${minute}:${second} ${ampm}`;
+  
+    return `${formattedDate} ${formattedTime}`;
+}
+
+const addToLog = (response, setConsoleLogs) => {
+    console.log("Adding to log");
+    const { message, time } = response;
+    const newLog = { message, time };
+    setConsoleLogs(prevConsoleLogs => [...prevConsoleLogs, newLog]);
+}
+
+export { convertTimeToString, addToLog };
