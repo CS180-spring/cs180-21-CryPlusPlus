cf90fb2ddc4b7f89df77f038ac4f90834f1cb2a8
daniel-u-c <74112469+daniel-u-c@users.noreply.github.com>
2023-05-29 00:46:20 -0700

    Implemented initial console design

--- crydb/components/Console.jsx
+++ crydb/components/Console.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useUserContext } from "@app/context/UserContext";
+import { convertTimeToString } from '@utils/helpers';
 
 const Console = () => {
-  return (
-    <div className="">
+  const { consoleLogs } = useUserContext();
 
+  return (
+    <div className="flex flex-col h-full p-8 gap-5">
+      <div className="flex flex-col gap-2">
+        {consoleLogs.map((response, i) => {
+          return (
+            <div className="flex flex-row gap-4 hover:bg-figma-lightpink w-full" key={i}>
+                <div className='font-bold'>
+                    {convertTimeToString(response.time)}:
+                </div>
+                <div className=''>
+                    {response.message} 
+                </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
