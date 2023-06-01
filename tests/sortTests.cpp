bf4b78b354db9d7782eb05253dc159a6e1e0b709
Zinal Patel <zinalp19@gmail.com>
2023-06-01 16:46:01 -0700

    Updated sort test file

--- tests/sortTests.cpp
+++ tests/sortTests.cpp
@@ -1,45 +1,85 @@
 #include <gtest/gtest.h>
 #include "../src/Sort.h"
 #include "../src/Document.h"
-#include "../src/Collection.h"
+#include "../src/Collections.h"
 // #include "../src/Document.h"
+#include <vector>
+#include <string>
 
 using json = nlohmann::ordered_json;
 
+using namespace std;
 
-// NOT COMPLETE NEED TO REFORMAT
+using namespace testing; 
+
+class SortTest : public Test {
+    protected: 
+        vector <Document> testcollection; 
+
+        void SetUp() override {
+            Document doc1({{"name", "John"}, {"age", 30}, {"location", "New York"}});
+            Document doc2({{"name", "Chloe"}, {"age", 25}, {"location", "San Francisco"}});
+            Document doc3({{"name", "Bob"}, {"age", 27}, {"location", "Seattle"}});
+
+            testcollection.push_back(doc1);
+            testcollection.push_back(doc2);
+            testcollection.push_back(doc3); 
+        }
+};
 
 
-TEST(SortTest, sorting) {
-    std::sort_field = "ascending"; 
-    json test1 = 
-        [
-            {
-                "Name": "Charlie",
-                "Age": 25,
-                "Location": "Los Angeles"
-            },
-            {
-                "Name": "Echo",
-                "Age": 20,
-                "Location": "New York"
-            }
-        ]
+TEST(SortTest, AscendingByName) {
+    string sort_field = "ASCENDING"; 
+    string sort_type = "name"; 
+    document_sort(testcollection, sort_field, sort_type); 
 
-json test2 = 
-[
-    {
-        "Name": "Delta",
-        "Age": 25,
-        "Location": "Los Angeles"
-    },
-    {
-        "Name": "Bob",
-        "Age": 20,
-        "Location": "New York"
-    }
-]
+    EXPECT_EQ(testcollection.front(), "doc3");
+    EXPECT_EQ(testcollection.back(), "doc1");
+}
 
+TEST(SortTest, DescendingByName) {
+    string sort_field = "DESCENDING"; 
+    string sort_type = "name"; 
+    document_sort(testcollection, sort_field, sort_type); 
+
+    EXPECT_EQ(testcollection.front(), "doc1");
+    EXPECT_EQ(testcollection.back(), "doc3");
+}
+
+TEST(SortTest, AscendingByAge) {
+    string sort_field = "ASCENDING"; 
+    string sort_type = "age"; 
+    document_sort(testcollection, sort_field, sort_type); 
+
+    EXPECT_EQ(testcollection.front(), "doc2");
+    EXPECT_EQ(testcollection.back(), "doc1");
+}
+
+TEST(SortTest, DescendingByAge) {
+    string sort_field = "DESCENDING"; 
+    string sort_type = "age"; 
+    document_sort(testcollection, sort_field, sort_type); 
+
+    EXPECT_EQ(testcollection.front(), "doc1");
+    EXPECT_EQ(testcollection.back(), "doc2");
+}
+
+TEST(SortTest, AscendingByLocation) {
+    string sort_field = "ASCENDING"; 
+    string sort_type = "location"; 
+    document_sort(testcollection, sort_field, sort_type); 
+
+    EXPECT_EQ(testcollection.front(), "doc1");
+    EXPECT_EQ(testcollection.back(), "doc2");
+}
+
+TEST(SortTest, DescendingByLocation) {
+    string sort_field = "DESCENDING"; 
+    string sort_type = "location"; 
+    document_sort(testcollection, sort_field, sort_type); 
+
+    EXPECT_EQ(testcollection.front(), "doc3");
+    EXPECT_EQ(testcollection.back(), "doc1");
 }
 
 
