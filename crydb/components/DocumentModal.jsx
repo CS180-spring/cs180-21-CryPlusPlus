1361c3b98036e842ab63c7bcefa0c57721f3b04a
daniel-u-c <dcast162@ucr.edu>
2023-06-05 11:07:25 -0700

    Updates

--- crydb/components/DocumentModal.jsx
+++ crydb/components/DocumentModal.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import JSONView from "react-json-view";
 import { useState } from "react";
+import { useUserContext } from "@app/context/UserContext";
+import { addToLog, updateTable } from "@utils/helpers";
 
 const DocumentModal = ({ visible, setVisible, jsonString }) => {
   if (!visible) return null;
+  const { userCollections, setConsoleLogs, setDataColumns, setTableData } =
+    useUserContext();
 
   const [displayObjectSize, setDisplayObjectSize] = useState(false);
   const [displayDataTypes, setDisplayDataTypes] = useState(false);
@@ -13,6 +17,27 @@ const DocumentModal = ({ visible, setVisible, jsonString }) => {
   const UUID = data.__UUID__;
   console.log(UUID);
   delete data["__UUID__"];
+
+  const handleUpload = async (e) => {
+    try {
+      // Send POST request to create the collection
+      const response = await fetch("http://localhost/createCollection", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(e),
+      });
+
+      const data = JSON.parse(await response.text());
+      const table = data.data;
+      console.log("Response from localhost:", data);
+      updateTable(table.data, setTableData);
+      addToLog(data, setConsoleLogs);
+    } catch (error) {
+      console.error("Error fetching from localhost:", error);
+    }
+  };
 
   const onDelete = (e) => {
     let fieldPath = null;
@@ -24,6 +49,7 @@ const DocumentModal = ({ visible, setVisible, jsonString }) => {
       newValue: null,
     };
     console.log("deleting", requestData);
+    handleUpload(requestData);
   };
   const onEdit = (e) => {
     let fieldPath = "";
@@ -36,6 +62,7 @@ const DocumentModal = ({ visible, setVisible, jsonString }) => {
       newValue: e.new_value,
     };
     console.log("editted", requestData);
+    handleUpload(requestData);
   };
   const onAdd = (e) => {
     let fieldPath = "";
@@ -48,6 +75,7 @@ const DocumentModal = ({ visible, setVisible, jsonString }) => {
       newValue: e.new_value,
     };
     console.log("adding", requestData);
+    handleUpload(requestData);
   };
 
   return (
