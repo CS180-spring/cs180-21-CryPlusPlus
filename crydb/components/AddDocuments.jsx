5c414d4add58a4a5aafa6319bbf201dce0a5d16b
btang031 <btang031@ucr.edu>
2023-06-05 12:13:35 -0700

    Added fileupload with file verification for json files

--- crydb/components/AddDocuments.jsx
+++ crydb/components/AddDocuments.jsx
@@ -20,6 +20,89 @@ const AddDocuments = () => {
   };
 
 
+const handleUpload = async () => {
+  if (!files || files.length === 0) {
+    setAddDocument(false);
+    return;
+  }
+
+  let isInvalidFile = false; // Flag to indicate if the file is not valid
+
+  // Convert the files to base64 strings and send them to the backend
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    const base64 = await new Promise((resolve) => {
+      reader.onloadend = () => resolve(reader.result);
+    });
+
+    // Create a JSON object
+    const json = {
+      filename: file.name,
+      data: base64,
+    };
+
+    // Check the file type
+    if (file.type !== "application/json") {
+      console.error("Invalid file type");
+      isInvalidFile = true; // Set the flag if the file type is invalid
+      break;
+    }
+
+    // Convert the JSON object to a string
+    const jsonString = JSON.stringify(json);
+    console.log(jsonString);
+
+    // Attempt to parse the JSON string
+    let parsedJson;
+
+    try {
+      parsedJson = JSON.parse(jsonString);
+      if (!parsedJson || typeof parsedJson !== "object") {
+        throw new Error("Invalid JSON file");
+        return;
+	}
+      // Send the JSON string to the backend
+      try {
+        const response = await fetch("http://localhost/uploadFile", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: jsonString,
+        });
+
+        // Log the response from the server
+        const data = JSON.parse(await response.text());
+        const table = data.data;
+        console.log("Response from localhost:", data);
+        updateTable(table.data, setTableData);
+        setDataColumns(table.columns);
+        addToLog(data, setConsoleLogs);
+      } catch (error) {
+        console.error("Error fetching from localhost:", error);
+        isInvalidFile = true; // Set the flag if there is an error
+        break;
+      }
+    } catch (error) {
+      console.error("Invalid JSON file:", error);
+      isInvalidFile = true; // Set the flag if the JSON is invalid
+      break;
+    }
+  }
+
+  if (isInvalidFile) {
+    // Handle the case when the file is not a valid JSON or has an invalid type
+    // For example, show an error message to the user
+    console.log("Invalid JSON file");
+    return;
+  }
+
+  if (!isInvalidFile) {
+    setAddDocument(false);
+  }
+};
 
 
     setAddDocument(false);
